5eec88d059b381066fcf3530ae69e581606bb3a4
Wbofeng <1464226975@qq.com>
2018-03-24 13:49:04 +0800

    let-const-var

--- codeSegment/let-const-var.js
+++ codeSegment/let-const-var.js
@@ -37,3 +37,62 @@
 // }
 
 // SyntaxError declared
+
+// function f() {
+//     var a = 0;
+//     function l() {
+//         var a = 2;
+//         function m() {
+//             console.log(a);
+//         }
+//         m();
+//     }
+//     l();
+// }
+// f();
+
+// 2
+
+// function f() {
+//     var a = 0;
+//     function l() {
+//         function m() {
+//             console.log(a);
+//         }
+//         m();
+//         var a = 2;
+//     } 
+//     l();
+// }
+// var a = 1;
+// f();
+// console.log(a);
+
+// undefined
+// undefined
+
+// var a = 4;
+// (function I( def ) {
+//     def(2);
+//     console.log(1);
+// })(function def(global) {
+//     var a = 3;
+//     console.log(a);
+//     console.log(global);
+// })
+
+// 3
+// 2
+// 1
+
+// function foo() {
+//     console.log(a);
+// }
+// function bar() {
+//     var a = 3;
+//     foo();
+// }
+// var a = 2;
+// bar();
+
+// 2
