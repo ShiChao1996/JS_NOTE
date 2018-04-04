46e88f9473bf777d90864575ec5fba91e1290081
Wbofeng <1464226975@qq.com>
2018-04-04 23:44:55 +0800

    add value

---
+++ codeSegment/value.js
@@ -0,0 +1,14 @@
+// function foo() {
+//     console.log(0 === -0);
+// }
+// foo(); // true
+
+// function foo() {
+//     console.log(Infinity === Infinity);
+// }
+
+// foo(); // true
+
+// console.log(Object.is(-0, 0)); // false
+// console.log([][[]] === [][0]); // true
+console.log(([][[]] + '')[[] + 0] === "u");
